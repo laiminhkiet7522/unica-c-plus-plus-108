766586edd6738229de111c5904254755fdc02416
laiminhkiet7522 <laiminhkiet07052002@gmail.com>
2024-04-20 20:25:42 +0700

    Bai 80

---
+++ unica_buoi_80/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+int main()
+{
+    char s1[25];
+    char s2[25];
+
+    cout<<"Nhap chuoi s1: ";
+    cin.getline(s1,25);
+    strcpy(s2,s1);
+    cout<<"\nChuoi s1 ban vua nhap la: ";
+    cout<<s1<<endl;
+    cout<<"Chuoi s2 coppy tu s1 la: ";
+    cout<<s2<<endl;
+
+    char str3[5];
+    strncpy(str3,s1,4);
+    cout<<"\nChuoi s3 coppy tu s1 la: ";
+    cout<<str3<<endl;
+    return 0;
+}
